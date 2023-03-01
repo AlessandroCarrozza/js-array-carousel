1437d0028b3d63a4a5a09a8b9218489b13b6c820
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-02 00:24:31 +0100

    Bonus completato

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -11,34 +11,49 @@ const imagesCarousel = [
 const sliderDom = document.getElementById("slider");
 const prevDom = document.getElementById("prev");
 const nextDom = document.getElementById("next");
+const thumbsDom = document.getElementById("thumbs");
 
 let sliderContent = "";
+let thumbsContent = "";
 
 for (let i = 0; i < imagesCarousel.length; i++) {
     
     let divImage = `<div class="image">
                         <img src="${imagesCarousel[i]}">
                     </div>`;
+
+    let divThumbs = `<div class="thumbs-images brightness">
+                        <img class="thumbs-img" src="${imagesCarousel[i]}">
+                     </div>`;                
     
     sliderContent += divImage;
+    thumbsContent += divThumbs;
+
 }
 
 
+
 sliderDom.innerHTML = sliderContent;
+thumbsDom.innerHTML = thumbsContent;
 
 // array con tutte le img
 const slides = document.getElementsByClassName("image");
+const thumbsImageDom = document.getElementsByClassName("thumbs-images");
 
+console.log(thumbsImageDom);
 
 let activeImage = 0;
-slides[0].classList.add("show");
-
+slides[activeImage].classList.add("show");
+thumbsImageDom[activeImage].classList.remove("brightness");
+thumbsImageDom[activeImage].classList.add("activeThumbs");
 
 nextDom.addEventListener("click" , 
 
     function () {
 
         slides[activeImage].classList.remove('show');
+        thumbsImageDom[activeImage].classList.add("brightness");
+        thumbsImageDom[activeImage].classList.remove('activeThumbs');
      
         if (activeImage == slides.length -1) {
             activeImage = 0;
@@ -47,6 +62,8 @@ nextDom.addEventListener("click" ,
         }
 
         slides[activeImage].classList.add('show');
+        thumbsImageDom[activeImage].classList.remove("brightness");
+        thumbsImageDom[activeImage].classList.add("activeThumbs");
 
     });
 
@@ -56,6 +73,8 @@ prevDom.addEventListener("click" ,
     function () {
 
         slides[activeImage].classList.remove('show');
+        thumbsImageDom[activeImage].classList.add("brightness");
+        thumbsImageDom[activeImage].classList.remove('activeThumbs');
      
         if (activeImage == 0) {
             activeImage = (slides.length - 1);
@@ -64,6 +83,8 @@ prevDom.addEventListener("click" ,
         }
 
         slides[activeImage].classList.add('show');
+        thumbsImageDom[activeImage].classList.remove("brightness");
+        thumbsImageDom[activeImage].classList.add("activeThumbs");
          
     });    
 
